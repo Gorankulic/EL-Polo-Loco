c027374396ea9945715d8d72c623db2e6dc3ac57
Goran <gorankulic@outlook.comm>
2023-10-04 21:46:02 +0200

    small chicken has been added

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
 
-    console.log('My character is', world.character);
+
 }
 
 
---
+++ models/small-chicken.class.js
@@ -0,0 +1,41 @@
+class SmallChickens extends MovableObject {
+
+    y = 390;
+    height = 50;
+    width = 40;
+
+
+
+
+    RUNNING_IMAGES_FOR_SMALL_CHICKEN = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+
+
+    ];
+
+
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+
+
+        this.loadImages(this.RUNNING_IMAGES_FOR_SMALL_CHICKEN);
+
+        this.x = 200 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() * 0.25;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 100);
+    }
+
+}
