f2f4e4a69bad1b4b4df4052cb776c14b2830733d
Goran <gorankulic@outlook.comm>
2023-10-14 22:26:24 +0200

    on display touch js logic is being implemented, second part but its not working like its supposed to work

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -12,6 +12,7 @@ class Keyboard {
         this.bindKeyPressEvents();
         this.bindBtsPressEvents();
     }
+
     bindBtsPressEvents() {
         document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
             e.preventDefault();
@@ -22,8 +23,89 @@ class Keyboard {
             e.preventDefault();
             this.LEFT = false;
         })
+        document.getElementById('btnRight').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.RIGHT = true;
+        });
 
+        document.getElementById('btnRight').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.RIGHT = false;
+        })
 
+        document.getElementById('btnJump').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.SPACE = true;
+        });
+
+        document.getElementById('btnJump').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.SPACE = false;
+        })
+
+        document.getElementById('btnThrow').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.D = true;
+        });
+
+        document.getElementById('btnThrow').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.D = false;
+        })
 
     }
+
+    bindKeyPressEvents() {
+
+
+        window.addEventListener("keydown", (e) => {
+            if (e.keyCode == 37) {
+                keyboard.LEFT = true;
+            }
+            if (e.keyCode == 38) {
+                keyboard.UP = true;
+            }
+            if (e.keyCode == 39) {
+                keyboard.RIGHT = true;
+            }
+            if (e.keyCode == 40) {
+                keyboard.DOWN = true;
+            }
+            if (e.keyCode == 32) {
+                keyboard.SPACE = true;
+            }
+            if (e.keyCode == 68) {
+                setTimeout(() => {
+                    keyboard.D = true;
+                }, 500);
+
+            }
+        });
+
+        window.addEventListener("keyup", (e) => {
+            if (e.keyCode == 37) {
+                keyboard.LEFT = false;
+            }
+            if (e.keyCode == 38) {
+                keyboard.UP = false;
+            }
+            if (e.keyCode == 39) {
+                keyboard.RIGHT = false;
+            }
+            if (e.keyCode == 40) {
+                keyboard.DOWN = false;
+            }
+            if (e.keyCode == 32) {
+                keyboard.SPACE = false;
+            }
+            if (e.keyCode == 68) {}
+            setTimeout(() => {
+                keyboard.D = false;
+            }, 500);
+        });
+
+    }
+
+
+
 }
