83fbdafdc899ad051c86203121d41032ac50c21f
Goran <gorankulic@outlook.comm>
2023-10-14 21:49:12 +0200

    on display touch js logic is being implemented

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -5,4 +5,25 @@ class Keyboard {
     DOWN = false;
     SPACE = false;
     D = false;
+    THROW_REQUEST_STOP = new Date().getTime();
+    THROW_REQUEST_START = 0;
+
+    constructor() {
+        this.bindKeyPressEvents();
+        this.bindBtsPressEvents();
+    }
+    bindBtsPressEvents() {
+        document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.LEFT = true;
+        });
+
+        document.getElementById('btnLeft').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.LEFT = false;
+        })
+
+
+
+    }
 }
