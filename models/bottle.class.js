25072138ec41d0a20db75b130e3ce106bbb75dbd
Gorankulic <gorankulic@outlook.com>
2024-03-07 23:43:13 +0100

    bottle flying animation is added and it´s working

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -8,7 +8,6 @@ class Bottle extends MovableObject {
         'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
         'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
     ];
-
     constructor() {
         super();
         const randomIndex = Math.floor(Math.random() * this.BOTTLE_IMAGES.length);
--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,26 +1,93 @@
 class ThrowableObject extends MovableObject {
+    FLYING_BOTTLE_IMAGES = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ];
+    BOTTLE_SPLASH_IMAGES = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+    ];
+    thrownBottle = false;
 
     constructor(x, y, direction = 'right') {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.FLYING_BOTTLE_IMAGES);
+        this.loadImages(this.BOTTLE_SPLASH_IMAGES);
 
         this.x = x;
         this.y = y;
         this.height = 60;
         this.width = 50;
         this.throw(direction);
+        this.animate();
     }
 
     throw (direction) {
-        this.speedY = 15;
-        this.applyGravity();
+            this.thrownBottle = true;
+            this.speedY = 15;
+            this.applyGravity();
+            setInterval(() => {
+
+                if (direction === 'right') {
+                    this.x += 2;
+                } else {
+                    this.x -= 2;
+                }
+
+            }, 5);
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////
+    animate() {
+
         setInterval(() => {
-            
-    if (direction === 'right') {
-        this.x += 2;
-    } else {
-        this.x -= 2;
+            // Handle animation states
+            if (this.thrownBottle == true) {
+                this.playAnimation(this.FLYING_BOTTLE_IMAGES);
+            } else if (this.isHurt()) {
+                this.playAnimation(this.BOTTLE_SPLASH_IMAGES);
+            } else if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else if (this.isSleeping()) {
+                this.playAnimation(this.IMAGES_SLEEPING);
+            } else if (this.isBored()) {
+                this.playAnimation(this.IMAGES_IDLE);
+            } else {
+                // Default walking animation if character is moving left or right
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
+            }
+        }, 50);
     }
-    
-        }, 5);
+    jump() {
+        this.speedY = 16;
     }
+    secondJump() {
+        this.speedY = 15; // Initial jump boost
+
+    }
+    isBored() {
+        const currentTime = new Date().getTime();
+        return this.lastMovedTimestamp && (currentTime - this.lastMovedTimestamp) > 500;
+    }
+    isSleeping() {
+            const currentTime = new Date().getTime();
+            return this.lastMovedTimestamp && (currentTime - this.lastMovedTimestamp) > 5000;
+        }
+        // Inside your Character class:
+    moveLeft() {
+        this.x -= this.speed;
+        this.lastDirection = 'left';
+    }
+
+    moveRight() {
+        this.x += this.speed;
+        this.lastDirection = 'right';
+    }
+
+
 }
