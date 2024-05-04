bf3565b9372d1bcf2ebedb0ce9029c28dc723fae
Gorankulic <gorankulic@outlook.com>
2024-05-04 21:42:49 +0200

    endscreen image is added

---
+++ models/end-game-screen.class.js
@@ -0,0 +1,49 @@
+class endGameHealthScreen extends DrawableObject {
+    END_SCREEN_IMAGE = [
+        `img/9_intro_outro_screens/game_over/game over!.png`
+    ];
+
+    constructor() {
+        super();
+        this.loadImage(this.END_SCREEN_IMAGE);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
