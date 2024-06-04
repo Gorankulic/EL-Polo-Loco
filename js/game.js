0be93d4ced77cb0624fece1687fabd668f5af86e
Goran Kulic <gorankulic@outlook.com>
2024-06-04 22:20:56 +0200

    game restart feature is added

--- js/game.js
+++ js/game.js
@@ -6,4 +6,11 @@ function init() {
     keyboard = new Keyboard();
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
+
+    // Add the event listener for the restart image
+    document.getElementById('restartGameIcon').addEventListener('click', reloadPage);
+}
+
+function reloadPage() {
+    location.reload();
 }
