83da183927a41574ea6fd8bfdac5e127e482183e
Goran Kulic <gorankulic@example.com>
2024-06-14 01:23:35 +0200

    game start screen is added and it removes itseld, but game chacarters are moving in background uncontrolably

--- js/game.js
+++ js/game.js
@@ -3,12 +3,20 @@ let world;
 let keyboard;
 
 function init() {
+
     keyboard = new Keyboard();
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
 
     // Add the event listener for the restart image
     document.getElementById('restartGameIcon').addEventListener('click', reloadPage);
+
+
+
+    document.getElementById('startScreen').style.display = 'none';
+
+
+
 }
 
 function reloadPage() {
