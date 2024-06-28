6453e906eaffb4729debdfdbdb68cf592c7d9d82
Goran Kulic <gorankulic@example.com>
2024-06-28 12:57:30 +0200

    character is now forward

--- models/world.class.js
+++ models/world.class.js
@@ -362,11 +362,13 @@ class World {
         // Translate once for drawing objects relative to the camera
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.backgroundObjects);
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.level.clouds);
+
+
         this.addObjectsToMap(this.level.bottle);
         this.addObjectsToMap(this.level.coins);
         this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.addToMap(this.character);
 
         this.addObjectsToMap(this.throwableObjects);
 
